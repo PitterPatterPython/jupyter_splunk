001ce85428a59b16847750d93779c42eac0bd635
John Omernik <darkness@cybermac.local>
2021-10-04 10:09:31 -0500

    Blah

--- splunk_core/splunk_base.py
+++ splunk_core/splunk_base.py
@@ -144,7 +144,7 @@ class Splunk(Integration):
         m = re.search("\d{1,2}\/\d{1,2}\/\d{4}", intime)
 
         if m:
-            tmp_dt = datetime.datetime.strptime(intime)
+            tmp_dt = datetime.datetime.strptime(intime, "%m/%d/%Y:%H:%M:%S")
             outtime = tmp_dt.strftime("%Y-%m-%dT%H:%M:%S")
         else:
             outtime = intime
