11bdd96410ff4420bac3a8d9dceb39ef2bc1567b
Rob D'Aveta <rob.daveta@gmail.com>
2023-10-26 10:21:06 -0400

    updated print statements in splunk_base to use markdown

--- splunk_core/splunk_base.py
+++ splunk_core/splunk_base.py
@@ -2,6 +2,8 @@
 
 from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic, line_cell_magic)
 from splunk_core._version import __desc__
+import jupyter_integrations_utility as jiu
+
 @magics_class
 class Splunk(Magics):
     # Static Variables
@@ -20,17 +22,17 @@ class Splunk(Magics):
         # Check namespace for integration and addon dicts
         if "jupyter_loaded_integrations" not in self.shell.user_ns:
             if self.debug:
-                print("jupyter_loaded_integrations not found in ns: adding")
+                jiu.displayMD("**[ Dbg ]** jupyter_loaded_integrations not found in ns: adding")
             self.shell.user_ns['jupyter_loaded_integrations'] = {}
         if "jupyter_loaded_addons" not in self.shell.user_ns:
             if self.debug:
-                print("jupyter_loaded_addons not found in ns: adding")
+                jiu.displayMD("**[ Dbg ]** jupyter_loaded_addons not found in ns: adding")
             self.shell.user_ns['jupyter_loaded_addons'] = {}
 
         # Check to see if our name_str is in loaded integrations (it shouldn't be)
         if self.name_str in self.shell.user_ns['jupyter_loaded_integrations']:
-            print(f"Potenital Multiverse collision of names: {self.name_str}")
-            print(self.shell.user_ns['jupyter_loaded_integrations'])
+            jiu.displayMD(f"**[ ! ]** Potenital Multiverse collision of names: {self.name_str}")
+            jiu.displayMD(f"{self.shell.user_ns['jupyter_loaded_integrations']}")
         else:
             # This is where add our base version
             self.shell.user_ns['jupyter_loaded_integrations'][self.name_str] = f"{self.name_str}_base"
@@ -44,16 +46,16 @@ class Splunk(Magics):
     @line_cell_magic
     def splunk(self, line, cell=None):
         if not self.name_str in self.shell.user_ns['jupyter_loaded_integrations']:
-            print(f"Somehow we got here and {self.name_str} is not in loaded integrations - Unpossible")
+            jiu.displayMD(f"**[ ! ]** Somehow we got here and {self.name_str} is not in loaded integrations - Unpossible")
         else:
             if self.shell.user_ns['jupyter_loaded_integrations'][self.name_str] != f"{self.name_str}_base":
-                print(f"We should only get here with a {self.name_str}_base state. Currently for {self.name_str}: {self.shell.user_ns['jupyter_loaded_integrations'][self.name_str]}")
+                jiu.displayMD(f"**[ ! ]** We should only get here with a {self.name_str}_base state. Currently for {self.name_str}: {self.shell.user_ns['jupyter_loaded_integrations'][self.name_str]}")
             else:
                 if self.debug:
-                    print(f"Loading full {self.name_str} from base")
+                    jiu.displayMD(f"**[ Dbg ]** Loading full {self.name_str} from base")
                 full_load = f"from {self.name_str}_core.{self.name_str}_full import {self.name_str.capitalize()}\n{self.name_str}_full = {self.name_str.capitalize()}(ipy, debug={str(self.debug)})\nipy.register_magics({self.name_str}_full)\n"
                 if self.debug:
-                    print("Load Code: {full_load}")
+                    jiu.displayMD("**[ Dbg ]** Load Code: {full_load}")
                 self.shell.ex(full_load)
                 self.shell.user_ns['jupyter_loaded_integrations'][self.name_str] = f"{self.name_str}_full"
                 self.shell.run_cell_magic(self.name_str, line, cell)
